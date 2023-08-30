5b06260ea9d614d6132b753af9749b6d315a9306
Beatriz Doravante <139794497+trizdoravante@users.noreply.github.com>
2023-08-30 00:09:43 -0300

    Add files via upload

--- HelloWorldController.java
+++ HelloWorldController.java
@@ -11,5 +11,35 @@ public class HelloWorldController {
 	public String helloWorld() {
 		return "Hello World!!!";
 	}
+		@GetMapping("/BSM")
+			public String ListaBSM() {
+			return "<pre>/n"+
+			"Lista de BSM Generation Brasil/n"+
+					"Mentalidade de Crescimento/n"+
+			        "Persistência/n"+
+					"Responsabilidade Pessoal/n"+
+			        "Orientação ao Futuro/n"+
+					"Comunicação/n"+
+			        "Orientação ao Detalhe/n"+
+					"Proatividade/n"+
+			        "Trabalho em Equipe/n"+
+					"<pre>/n";
+		}
+			@GetMapping("/objSemana")
+				
+			public String ListaSemana(){
+				return "<pre>/n"+
+			"Objetivos de aprendizagem da semana:/n"+
+						"Revisar os conteúdos apresentados durante a semana./n"+
+			            "Fazer exercícios de MySQL./n"+
+						"Ler o Cookbook e fazer anotações./n"+
+			            "Verificar se estou de fato em dia com todos os exercícios./n"+
+						"<pre>/n";
+						
+			}
+			
+		}
+	
+	
+	
 
-}
